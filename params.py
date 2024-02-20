bffc87564d111cbf56e3fb5d962a166679c4540f
Ananya HR <ananya.ramesh@happiestminds.com>
2024-02-20 14:21:21 +0530

    Parameters

---
+++ params.py
@@ -0,0 +1,12 @@
+'''Snowflake Configuration'''
+user = 'ananya'
+password ='Raohedli2329#'
+account = 'eleieug-za95780'
+database = 'callcap'
+schema = 'callcap_data'
+
+'''Slack Configuration'''
+slack_token = "xoxb-6597753635431-6625048338209-cXLRFFZc0cBjfiM7fVFZiBAp"
+slack_channel_success = "#test_callcap_success"
+slack_channel_failure = "#test_callcap_error"
+

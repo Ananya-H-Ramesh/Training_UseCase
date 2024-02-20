d40ab1adc06b032d60cac851fc12c190262bd84c
Ananya HR <ananya.ramesh@happiestminds.com>
2024-02-20 14:16:21 +0530

    Constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+'''Path where Csv data is stored in Local'''
+csv_path = r"C:/Users/ananya.ramesh/Phase_1/training_phase1/callcap/Data/20190101-20190111_CallCapCalls.csv"
+
+'''Path where Json data is stored in Local'''
+json_path = r"C:/Users/ananya.ramesh/Phase_1/training_phase1/callcap/Data"
+
+'''Staging Table name'''
+staging_callcap = 'staging_callcap'
+
+'''Final Table where data is loaded'''
+final_callcap = 'callcap'
+
+
